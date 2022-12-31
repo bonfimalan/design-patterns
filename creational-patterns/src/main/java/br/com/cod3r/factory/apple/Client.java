9e36e901a8283cc7c8ccc666228b6745978c2047
bonfimalan <alanbonfim2000@gmail.com>
2022-12-30 22:54:19 -0300

    Simple factory implemented

--- creational-patterns/src/main/java/br/com/cod3r/factory/apple/Client.java
+++ creational-patterns/src/main/java/br/com/cod3r/factory/apple/Client.java
@@ -3,7 +3,9 @@ package br.com.cod3r.factory.apple;
 import br.com.cod3r.factory.apple.factory.IPhoneXFactory;
 import br.com.cod3r.factory.apple.halfsimplefactory.IPhone11HalfSimpleFactory;
 import br.com.cod3r.factory.apple.model.IPhone;
+import br.com.cod3r.factory.apple.model.IPhoneGeneration;
 import br.com.cod3r.factory.apple.model.IPhoneLevel;
+import br.com.cod3r.factory.apple.simplefactory.SimpleIPhoneFactory;
 import br.com.cod3r.factory.apple.factory.IPhone11Factory;
 
 public class Client {
@@ -29,5 +31,13 @@ public class Client {
 		System.out.println("\n\n### Ordering an iPhone 11 pro");
 		IPhone ipHS11P = iPhone11HalfSimpleFactory.orderIPhone(IPhoneLevel.HIGH_END);
 		System.out.println(ipHS11P);
+
+		System.out.println("\n\n\n### Using a simple factory");
+		System.out.println("### Ordering an iPhone X");
+		IPhone ipSX = SimpleIPhoneFactory.orderIPhone(IPhoneGeneration.IP_X, IPhoneLevel.SIMPLE);
+		System.out.println(ipSX);
+		System.out.println("\n\n### Ordering an iPhone XSMax");
+		IPhone ipSXSmax = SimpleIPhoneFactory.orderIPhone(IPhoneGeneration.IP_X,IPhoneLevel.HIGH_END);
+		System.out.println(ipSXSmax);
 	}
 }
