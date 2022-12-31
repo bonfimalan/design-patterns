6b03079b2bbeebe03bd704144f32e378a4238cf4
bonfimalan <alanbonfim2000@gmail.com>
2022-12-30 23:02:33 -0300

    Factory implemented in DB

--- creational-patterns/src/main/java/br/com/cod3r/factory/app/dbadapter/Client.java
+++ creational-patterns/src/main/java/br/com/cod3r/factory/app/dbadapter/Client.java
@@ -1,8 +1,16 @@
 package br.com.cod3r.factory.app.dbadapter;
 
+import br.com.cod3r.factory.app.dbadapter.db.DB;
+import br.com.cod3r.factory.app.dbadapter.factory.OracleDBFactory;
+import br.com.cod3r.factory.app.dbadapter.factory.PostgresDBFactory;
+
 public class Client {
 
 	public static void main(String[] args) {
+		DB oracle = new OracleDBFactory().getDatabase();
+		DB postgres = new PostgresDBFactory().getDatabase();
 
+		oracle.query("SELECT * FROM table");
+		postgres.query("SELECT * FROM table");
 	}
 }
